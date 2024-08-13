f97de66dd9ab4e92ff04d7d99fbd788657fa3113
Ritik Raj <ritik@air.local>
2024-08-13 15:16:43 +0530

    Minor fix

--- src/aws.ts
+++ src/aws.ts
@@ -3,9 +3,8 @@ import fs from "fs";
 
 // replace with your own credentials
 const s3 = new S3({
-    accessKeyId: "AKIAREAV3T6VJLEMDUDU",
-    secretAccessKey: "u3wGBLc7j1En36N7OoKXgxjgbRISIr5XeVHnADDo",
-    // endpoint: "https://e21220f4758c0870ba9c388712d42ef2.r2.cloudflarestorage.com"
+    accessKeyId: "",
+    secretAccessKey: "",
 })
 
 
