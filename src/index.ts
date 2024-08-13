688b2f3259e1a96750217fd65ef3cdd94b084d3f
Ritik Raj <ritik@air.local>
2024-08-13 14:43:20 +0530

    Create a function to get all the files recursively

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import cors from "cors";
+import simpleGit from "simple-git";
+import path from "path";
+import { generate } from "./utils";
+import { getAllFiles } from "./file";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/deploy", (req, res) => {
+app.post("/deploy", async (req, res) => {
     const repoUrl = req.body.repoUrl;
-    console.log(`Deploying repo ${repoUrl}`);
-
-    res.json({});
+    const id = generate();
+    await simpleGit().clone(repoUrl, path.join(__dirname,`output/${id}`));
+    
+    const files = getAllFiles(path.join(__dirname,`output/${id}`));
+    res.json({
+        id: id,
+    });
 });
 app.listen(3000);
